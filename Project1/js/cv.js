6c2bd8d90e54e33829d676cdbad52cbc24398c49
Cristian <crisal1395@gmail.com>
2020-10-06 22:26:25 -0600

    general changes

---
+++ Project1/js/cv.js
@@ -0,0 +1,25 @@
+$("#dropdown").click(function () {
+    var display = document.getElementById('dropdown-content').style.display;
+    if (display === "none") {
+        $("#dropdown-content").css("display", "block");
+    } else {
+        $("#dropdown-content").css("display", "none");
+    }
+
+});
+
+$(document).ready(function() {
+    $("#logo").addClass("rotate-animation"); 
+    });
+
+    var x = document.getElementById("logo");
+    x.addEventListener("animationend", myEndFunction);
+
+    function myEndFunction () {
+        $("#logo").addClass("fadeout-animation"); 
+        setTimeout(function () {
+            $("#logo").attr("src", "../images/logo.png");
+            $("#logo").addClass("fadein-animation"); 
+        }, 1000);
+      
+    }
